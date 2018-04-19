316d141885ac5a82799b267a282694fe2924d5fd
Ari <dominiquearia0@gmail.com>
2018-04-19 16:38:17 -0700

    Fixed User Edit page

--- regrEtsy/app/controllers/users_controller.rb
+++ regrEtsy/app/controllers/users_controller.rb
@@ -4,9 +4,40 @@ class UsersController < ApplicationController
     @user = User.all
   end
 
+  def new
+    @user = User.new
+  end
+
   def show
     @user = User.find_by(id: params[:id])
     render_404 unless @user
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.assign_attributes(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+
+  def user_params
+    params.require(:user).permit(:name, :username, :password, :email)
+  end
 end
