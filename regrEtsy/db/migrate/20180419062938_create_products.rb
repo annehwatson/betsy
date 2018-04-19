28c820c37ddb5d32cccbaa5c3d23ab810a096d3c
Kat <kat.eastman@gmail.com>
2018-04-18 23:52:35 -0700

    added session, product and categories controller, product model sans user reference.

---
+++ regrEtsy/db/migrate/20180419062938_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.float :price
+      t.integer :stock
+      t.text :url
+      
+      t.timestamps
+    end
+  end
+end
