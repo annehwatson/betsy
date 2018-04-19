8c36629170cf397346309ec399f6e90648771fe5
Anne <annehwatson@gmail.com>
2018-04-19 15:02:29 -0700

    categories index view

--- regrEtsy/app/controllers/categories_controller.rb
+++ regrEtsy/app/controllers/categories_controller.rb
@@ -1,20 +1,45 @@
 class CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all
+  end
+
   def new
+    @category = Category.new(name: params[:name])
   end
 
   def create
+    category = Category.new(category_params)
+    if category.save
+      redirect_to categories_path
+    end
   end
 
   def show
+    @category = Category.find(params[:id])
   end
 
   def edit
+    @category = Category.find(params[:id])
   end
 
   def update
+    category = Category.find(params[:id])
+    category.assign_attributes(category_params)
+    if category.save
+      redirect_to category_path(category)
+    end
   end
 
   def destroy
+    Category.destroy(params[:id])
+  end
+
+  private
+
+  def category_params
+    return
+    params.require(:category).permit(:name)
   end
 
 end
--- regrEtsy/config/routes.rb
+++ regrEtsy/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resources :products
 
-  resources :categories, except: [:index] do
+  resources :categories, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
     resources :products, only: [:index]
   end
 
