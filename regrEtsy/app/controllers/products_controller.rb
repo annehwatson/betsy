b4be9452532d3365f1a5f7926389e7a4b72c70b8
0velez0 <mariaivelez@gmail.com>
2018-04-19 16:07:14 -0700

    commit before remote merge

--- regrEtsy/app/controllers/products_controller.rb
+++ regrEtsy/app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :find_product, only: [:show, :edit, :update, :delete]
+  before_action :require_login, except: [:index, :show]
 
 
   def root
