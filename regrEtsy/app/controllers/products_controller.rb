2e4cbb675aa48f6f70859e2ef95ac34f2c4b0f5d
0velez0 <mariaivelez@gmail.com>
2018-04-19 16:48:41 -0700

    merge work from session_features branch; add user id to product create method

--- regrEtsy/app/controllers/products_controller.rb
+++ regrEtsy/app/controllers/products_controller.rb
@@ -17,6 +17,7 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
+    @product.user_id = @user.id
     if @product.save
       flash[:success] = "Product added successfully"
       redirect_to products_path
