7fcaf901c61f034d9dc551fbf77aa5fd1fae5a9a
Kat <kat.eastman@gmail.com>
2018-04-20 11:20:38 -0700

    add default to retired.

--- regrEtsy/db/seeds.rb
+++ regrEtsy/db/seeds.rb
@@ -1,3 +1,6 @@
+User.create!([
+  {name: nil, username: "Jane Doe", email: nil, password: "verysecure", uid: nil, provider: nil}
+])
 Category.create!([
   {name: "Time Machines"},
   {name: "Pharmaceuticals"},
@@ -12,7 +15,4 @@ Product.create!([
   {name: "Plan B", description: "", price: 27.49, stock: 5, url: "", user_id: nil, retired: false},
   {name: "White Out", description: "", price: 3.99, stock: 6, url: "", user_id: 1, retired: false},
   {name: "Tardis", description: "Dr. Who time machine", price: 2300000000.0, stock: 1, url: "https://cdn.pixabay.com/photo/2017/05/14/09/51/tardis-2311634_960_720.png", user_id: nil, retired: false}
-])
-User.create!([
-  {name: nil, username: "Jane Doe", email: nil, password: "verysecure", uid: nil, provider: nil}
 ])
