5d0f891e70edb41c4b14b705af35e942f35dcafc
Kat <kat.eastman@gmail.com>
2018-04-19 19:37:06 -0700

    fixed join table.

--- regrEtsy/app/controllers/products_controller.rb
+++ regrEtsy/app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   before_action :find_product, only: [:show, :edit, :update, :delete]
-  before_action :require_login, except: [:index, :show]
+  before_action :require_login, except: [:root, :index, :show]
 
 
   def root
