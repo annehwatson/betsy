d7671cadf1b5b0af8035e307b4623103a2b4691e
Ari <dominiquearia0@gmail.com>
2018-04-20 11:36:14 -0700

    Updating user controller test & login

--- regrEtsy/app/controllers/sessions_controller.rb
+++ regrEtsy/app/controllers/sessions_controller.rb
@@ -6,23 +6,27 @@ class SessionsController < ApplicationController
 
   def login
     @user = User.find_by(name: params[:user][:name])
-
-    if @user.nil?
-      flash[:alert] = "Must sign up to login"
-      redirect_to new_user_path(@user)
-    end
-
     if @user
       session[:user_id] = @user.id
       flash[:success] = "Successfully logged in as existing user #{@user.name}"
+      # if @user.nil?
+      #   flash[:alert] = "Must sign up to login"
+      #   redirect_to new_user_path(@user)
     else
-      flash.now[:failure] = "Could not log in"
-      @user.errors.messages
-      @user.name = "please enter a username"
-      render :new, status: :failure
-      return
+      @user = User.new(name: params[:user][:name])
+
+      if @user.save
+        session[:user_id] = @user.id
+        flash[:success] = "Successfully created new user #{@user.name} with ID #{@user.id}"
+      else
+        flash.now[:failure] = "Could not log in"
+        @user.errors.messages
+        @user.name = "please enter a username"
+        render :new, status: :failure
+        return
+      end
+      redirect_to user_path(@user)
     end
-    redirect_to root_path
   end
 
   def logout
