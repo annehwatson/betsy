7900187473dbbf78d6de714f64b5cd8f07414c0a
Ari <dominiquearia0@gmail.com>
2018-04-23 12:09:03 -0700

    Pushing session controller create

--- regrEtsy/app/controllers/sessions_controller.rb
+++ regrEtsy/app/controllers/sessions_controller.rb
@@ -33,9 +33,10 @@ class SessionsController < ApplicationController
         flash[:success] = "Logged in successfully"
         redirect_to root_path
       end
-      else
-        flash[:error] = "Could not log in"
-        redirect_to root_path
+      
+    else
+      flash[:error] = "Could not log in"
+      redirect_to root_path
     end
   end
 
