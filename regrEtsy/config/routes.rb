101cb714379a94f7bede6e73fe03ecc45d1964ca
Kat <kat.eastman@gmail.com>
2018-04-23 11:49:21 -0700

    last bits before post-weekend merge.

--- regrEtsy/config/routes.rb
+++ regrEtsy/config/routes.rb
@@ -13,8 +13,8 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#logout', as: 'logout'
   get "/auth/:provider/callback", to: "sessions#create", as: 'auth_callback'
 
-  resources :cart, only: [:show, :edit, :update]
-  post '/cart', to: 'carts#add_to_cart', as: :add_to_cart
+  resources :carts, only: [:show, :edit, :update]
+  post '/carts', to: 'carts#add_to_cart', as: :add_to_cart
   post '/add', to: 'sessions#add_cart_product', as: :add_product
 
   get 'carts/:id/checkout', to: 'carts#checkout', as: :checkout
