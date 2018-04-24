cb786cb561f35348cc4c359310eda42ad6381ca8
Anne <annehwatson@gmail.com>
2018-04-23 19:46:33 -0700

    numericality 1-5 review

--- regrEtsy/app/models/product.rb
+++ regrEtsy/app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
 has_and_belongs_to_many :categories
 has_many :orderitems
 has_many :included_in_orders, through: :orderitems, source: :order
+#has_many :reviews
 
 def sufficient_stock(quantity)
   self.stock >= quantity
