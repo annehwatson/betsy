c01ae37e75d0acb23097d42e8d5fc65663cc4187
0velez0 <mariaivelez@gmail.com>
2018-04-24 21:56:29 -0700

    merging master into branch- resolves merge conflict

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 require 'simplecov'
 require 'simplecov-console'
-SimpleCov.formatter = SimpleCov.formatter = SimpleCov::Formatter::Console
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::Console,
+])
 SimpleCov.start 'rails'
 ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../config/environment", __FILE__)
