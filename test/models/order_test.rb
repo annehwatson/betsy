15eb12073c9068365d641fd4da28fed369722b47
Kat <kat.eastman@gmail.com>
2018-04-25 12:10:33 -0700

    changed _product and messed with order test.

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,9 +1,16 @@
 require "test_helper"
 
 describe Order do
-  let(:order) { Order.new }
+  it 'can be created with valid data' do
+    old_order_count = Order.count
+    order = Order.new(status: :pending)
 
-  it "must be valid" do
-    value(order).must_be :valid?
+    order.must_be :valid?
+
+    order = order.save
+    result = order.valid?
+
+    result.must_be true
+
   end
 end
