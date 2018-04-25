840d286fa278d3db16bf9c2b86b472c64dcd8057
Kat <kat.eastman@gmail.com>
2018-04-25 12:39:47 -0700

    pairing with Anne to fix review display for product#show.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,18 +5,12 @@ class ReviewsController < ApplicationController
 
   def new
     #needs validation on session[:user_id] to check that they aren't reviewing their own product they sell; is user_id on product the seller?
-    @review = Review.new(product_id: @product)
+    @review = Review.new
   end
 
   def create
     @review = Review.new(review_params)
-    # @review.product = @product
-    # if @review.save
-    #   flash[:success] = "Successfully created review"
-    #   redirect_to product_path(@review.product)
-    # else
-    #   flash.now[:error] = "Could not create new review"
-    #   render :new, status: :bad_request
+    @review.product_id = @product.id
     if @review.save
       flash[:success] = "Successfully created review"
       redirect_to product_path(@product)
@@ -27,12 +21,6 @@ class ReviewsController < ApplicationController
   end
 
   private
-  # def find_product
-  #   @product = Product.find_by(id: params[:product_id])
-  #   unless @product
-  #     head :not_found
-  #   end
-  # end
 
   def review_params
     return params.require(:review).permit(:rating, :product_id, :review)
