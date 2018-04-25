99ac6410e3e901a64fe66c737fe59369265dbf74
Kat <kat.eastman@gmail.com>
2018-04-25 11:01:26 -0700

    maria's changes to routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#root'
 
-  resources :products
-  get '/products/:id/reviews/new', to: 'reviews#new', as: 'new_product_review'
-  resources :users
+ # TODO changed using nested routes instead - leaving these here so you can see what we had previously
+  # get '/products/:id/reviews/new', to: 'reviews#new', as: 'new_product_review'
+
   resources :products do
-    resources :users, only: [:index]
+   resources :reviews, only: [:new, :create]
   end
 
-  resources :reviews, only: [:new, :create]
+  resources :users do
+    resources :products, only: [:index]
+  end
 
   resources :categories do
     resources :products, only: [:index]
@@ -19,9 +21,9 @@ Rails.application.routes.draw do
   get "/auth/:provider/callback", to: "sessions#create", as: 'auth_callback'
   delete '/logout', to: 'sessions#logout', as: 'logout'
 
-  resources :carts, only: [:show, :update]
-  post '/carts/', to: 'carts#add_to_cart', as: :add_to_cart
-
+  resources :carts, only: [:show, :edit, :update]
+  post '/carts', to: 'carts#add_to_cart', as: :add_to_cart
+  post '/add', to: 'sessions#add_cart_product', as: :add_product
 
   get 'carts/:id/checkout', to: 'carts#checkout', as: :checkout
   post 'carts/:id/checkout', to: 'carts#finalize'
